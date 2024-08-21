c9c5bdb60a54c548a6948383c0d3288d2a750950
YushenHu0326 <yushenhuuu@gmail.com>
2024-08-21 19:21:43 -0400

    changed UI structure

--- Assets/Script/System/Image/WebCamFeed.cs
+++ Assets/Script/System/Image/WebCamFeed.cs
@@ -6,17 +6,28 @@ using UnityEngine.UI;
 public class WebCamFeed : MonoBehaviour
 {
     WebCamTexture tex;
+    RawImage image;
+    Color color;
+
+    void Start()
+    {
+        image = gameObject.GetComponent<RawImage>();
+        if (image != null)
+        {
+            color = image.color;
+        }
+    }
 
     // Start is called before the first frame update
     public bool StartVideo(string webCamName)
     {
         if (webCamName.Length == 0) return false;
 
-        RawImage image = gameObject.GetComponent<RawImage>();
         if (image != null)
         {
             tex = new WebCamTexture(webCamName, 700, 400, 30);
             image.texture = tex;
+            image.color = Color.white;
             tex.Play();
             return true;
         }
@@ -28,10 +39,11 @@ public class WebCamFeed : MonoBehaviour
     {
         if (webCamName.Length == 0) return false;
 
-        RawImage image = gameObject.GetComponent<RawImage>();
         if (image != null && tex != null)
         {
             tex.Stop();
+            image.texture = null;
+            image.color = color;
             return true;
         }
 
