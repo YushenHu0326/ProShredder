a82d7e7f79d3023d8184fae6197f27e10effd4a5
Yushen Hu <yushenhuuu@gmail.com>
2024-08-20 22:27:02 -0400

    audio manager added

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void StartPlaying(int deviceID)
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        if (Microphone.devices.Length > deviceID)
+        {
+            int minFreq, maxFreq, freq;
+            Microphone.GetDeviceCaps(Microphone.devices[deviceID], out minFreq, out maxFreq);
+            freq = Mathf.Min(44100, maxFreq);
+
+            audioSource.clip = Microphone.Start(Microphone.devices[deviceID], true, 10, freq);
+            audioSource.loop = true;
+
+            while (!(Microphone.GetPosition(Microphone.devices[deviceID]) > 0)) { }
+            audioSource.Play();
+        }
+    }
+}
